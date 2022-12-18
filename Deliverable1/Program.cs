fa0231b0d0cb7b1bb234632ada2167367a2af167
camdixon132 <camdixon132@gmail.com>
2022-12-18 14:10:25 -0500

    First commit

---
+++ Deliverable1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Deliverable1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many people are being fed?");
+        }
+    }
+}
